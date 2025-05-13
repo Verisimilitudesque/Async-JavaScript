bb36517f6f4e5b31db925abb9120365dadeb4fcc
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-13 09:47:22 -0600

    API test

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
 //Fetches the current location of the user
-navigator.geolocation.getCurrentPosition(position => {
-    console.log(position);
-});
+//navigator.geolocation.getCurrentPosition(position => {
+   // console.log(position);
+//});
 
-console.log("done");
+//console.log("done");
 
 // Testing the Promise API
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => reject(Error('Promie failed')), 1000);  
-});
+//const promise = new Promise((resolve, reject) => {
+  // navigator.geolocation.getCurrentPosition(resolve(position), reject(error)); 
+//});
 
-promise.then(() => console.log('success')).catch( error => console.error('error')).finally(() => console.log('done'));
+//promise
+  //.then(position => console.log(position))
+  //.catch(error => console.error(error))
+  //.finally(() => console.log('done'));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1");
