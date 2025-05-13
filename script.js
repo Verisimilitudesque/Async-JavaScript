f8995987b77fcce4e0ea3157cb253601d6c74a7b
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-13 09:16:47 -0600

    Testing promise api

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+//Fetches the current location of the user
 navigator.geolocation.getCurrentPosition(position => {
     console.log(position);
 });
 
 console.log("done");
+
+// Testing the Promise API
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(Error('Promie failed')), 1000);  
+});
+
+promise.then(() => console.log('success')).catch( error => console.error('error')).finally(() => console.log('done'));
